bc757e822c65588fa61fdb882aa351e5153541c1
Tesla Owusu <tassan03cfc@gmail.com>
2026-04-05 19:23:58 +0000

    dim_countries before location change

--- dbt/models/marts/core/dim_countries.sql
+++ dbt/models/marts/core/dim_countries.sql
@@ -1,13 +1,12 @@
 with medallists as (
     select * from {{ref("stg_Milano_26__medallists_winter26")}}
-)
+),
 
 countries as (
-    select distinct(
-        country_code,
-        country
-    ) 
-    from {{ref("stg_Milano_26__medallists_winter26")}}
+    select 
+        distinct country_code,
+        country 
+    from medallists
 )
 
 select * from countries 
